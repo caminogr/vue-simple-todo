94d5d07914bca2cbfe6e422276fa22e61d0a8d2f
kaminora <kaminoura9207@gmail.com>
2019-10-04 13:50:32 +0900

    :sparkles: Enable to add todos

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,39 @@
 <template>
-  <div id="app"></div>
+  <div id="app">
+    Input your todo:
+    <input
+      type="text"
+      class="todo-input"
+      v-model="inputText"
+      @keyup.enter="submit(inputText)"
+    />
+    <div v-for="todo in todos" :key="todo.id">
+      <p>message: {{ todo.title }}</p>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
   name: "app",
+  data: function() {
+    return {
+      todos: [],
+      inputText: ""
+    };
+  },
+  methods: {
+    submit: function(text) {
+      const id = this.todos.length + 1;
+      this.todos.push({ id, title: text });
+      this.inputText = "";
+    }
+  },
   components: {}
 };
 </script>
 
-<style>
+<style scoped>
 #app {
   font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
